6e296c1e46d177e7299fc850707d188e6b3be9d6
Kullai Reddy Kummetha <kullaireddy027@gmail.com>
2018-07-16 17:49:10 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -127,7 +127,7 @@ function endGame(){
     // show prompt
     let stars = $(".fa-star").length;
     vex.dialog.confirm({
-        message: `Congrats! You just won the game in ${timeCount} seconds with ${stars}/3 rating. Do you want to play again?`,
+        message: `Congratulations! You won the game in ${timeCount} seconds with ${moves} moves and ${stars}/3 rating. Do you want to play again?`,
         callback: function(value){
             if (value){
                 resetGame();
